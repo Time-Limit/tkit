413f4a029fa0d44ba131cd6e2cb14c75293eba34
tlimit <tlimit@outlook.com>
2018-06-18 02:05:12 -0400

    mv website cupid, cupid is cute and stupid, not cupid.

---
+++ cupid/cupid.lua
@@ -0,0 +1,10 @@
+ConfigTable = {
+	["http-port"] = 8080,
+	["https-port"] = 443,
+		["base-folder"] = "./blog/public/",
+	--	["base-folder"] = "./../core/certbot/cert/",
+	["default-file-name"] = "./index.html",
+	["default-logic-thread-count"] = 4,
+	["cert_path"] = "/etc/letsencrypt/live/www.tlimit.cn/cert.pem",
+	["pkey_path"] = "/etc/letsencrypt/live/www.tlimit.cn/privkey.pem",
+};
---
+++ cupid/cupidbase.h
@@ -0,0 +1,12 @@
+#ifndef _WEBSITEBASE_H_
+#define _WEBSITEBASE_H_
+
+#include <string>
+
+extern unsigned int default_http_port; 
+extern unsigned int default_https_port;
+extern std::string default_base_folder;
+extern std::string default_file_name;
+extern size_t default_logic_thread_count;
+
+#endif
